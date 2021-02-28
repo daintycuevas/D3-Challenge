2c0cd339de80c829926feebb87e1421b80eb4575
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-28 01:04:38 -0500

    update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,5 +1,60 @@
 // @TODO: YOUR CODE HERE!
-// Create a map object
-d3.csv("data.csv", (d) => {
+
+//get data from csv file
+function logData() {
+    d3.csv("data/data.csv", (d) => {
+        console.log(d)
+    })
+}
+
+logData();
+
+
+var dataHealth = d3.csv("data/data.csv", (d) => {
     console.log(d)
+    });
+
+
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from hours-of-tv-watched.csv
+d3.csv("data/data.csv").then(d => {
+    console.log(d)
+
+    d.forEach(function(rate) {
+        rate.healthcare = +rate.healthcare;
+        rate.poverty = +rate.poverty;
+        rate.smokes = +rate.smokes;
+        rate.age = +rate.age;
+    });
 })
+
+
+
+
