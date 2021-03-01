3b84cf4eb40ef562e4a210bff06be59cf2332a99
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-03-01 17:02:35 -0500

    update scatter plot

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -28,8 +28,8 @@ var chartMargin = {
 };
 
 //set dimensions for chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+var width = svgWidth - chartMargin.left - chartMargin.right;
+var height = svgHeight - chartMargin.top - chartMargin.bottom;
 
 //append svg to body of the page
 var svg = d3
@@ -44,12 +44,12 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 //read the data
-d3.csv("data/data.csv").then(data => {
-    console.log(data)
+d3.csv("data/data.csv").then(healthData => {
+    // console.log(data)
 
-    data.forEach(function(record) {
-        // rate.healthcare = +rate.healthcare;
-        // rate.poverty = +rate.poverty;
+    healthData.forEach(function(record) {
+        record.healthcare = +record.healthcare;
+        record.poverty = +record.poverty;
 
         record.age = +record.age;
         record.smokes = +record.smokes;
@@ -57,37 +57,48 @@ d3.csv("data/data.csv").then(data => {
 
     //add x axis
     var x = d3.scaleLinear()
-        .domain(d3.extent(data, d => d.age))
-        .range([0, chartWidth]);
-    svg.append("g")
-        .attr("transform", `translate(0, ${chartHeight})`)
-        .call(d3.axisBottom(x));
+        .domain(d3.extent(healthData, d => d.age))
+        .range([0, width]);
 
     //add y axis
     var y = d3.scaleLinear()
-        .domain([0, 30])
-        .range([chartHeight, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+        .domain([0, d3.max(healthData, d => d.smokes)])
+        .range([height, 0]);
+
+    var y2 = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.poverty)])
+        .range([height, 0]);
+        
+    //create axes
+    var bottomAxis = d3.axisBottom(x);
+    var leftAxis = d3.axisLeft(y);
+    var rightAxis = d3.axisRight(y2);
+
+    //append axes to the chartGroup
+    chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(bottomAxis);
+    chartGroup.append("g").call(leftAxis);
+    chartGroup.append("g").attr("transform", `translate(${width}, 0)`).call(rightAxis);
 
     //add dots
-    svg.append("g")
-        .selectAll("dot")
-        .data(data)
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(healthData)
         .enter()
         .append("circle")
-        .attr("cx", function (d, i) { 
-            return x(d.age); 
-        })
-        .attr("cy", function (d) { 
-            return y(d.smokes); 
-        })
-        .attr("r", 4)
-        .style("fill", "blue");
+        .attr("cx", d => x(d.age))
+        .attr("cy", d => y(d.smokes))
+        .attr("r", 8)
+        .style("fill", "lightblue")
+        .attr("opacity", ".5");
+        // .attr("stroke-width", ".2")
+        // .attr("stroke", "dark gray");
 
-    }).catch(function(error) {
-        console.log(error);
+
+
     })
+    
+    // .catch(function(error) {
+    //     console.log(error);
+    // })
 
 
 
